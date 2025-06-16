d58e433972d26653821194911d2b73fd62fa3cf7
hazbiin <hzbi717@gmail.com>
2025-06-16 21:36:50 +0530

    refactor: implement task list view with localStorage

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,39 @@
 <script setup>
 
-  import { ref } from 'vue';
+  import { ref, onMounted, watch} from 'vue';
 
   import Header from './components/Header.vue';
   import TaskInputContainer from './components/TaskInputContainer.vue';
   import TodoListContainer from './components/TodoListContainer.vue';
 
-
-  // need to continue with the localstorage implemtation here
+  // tasks reactive variable
   const tasks = ref([]);
+
+
+  // get the savedTasks from localStorage at onMounted lifecycle hook.
+  onMounted(() => {
+    const savedTasks = localStorage.getItem('tasks');
+    if(savedTasks) {
+      tasks.value = JSON.parse(savedTasks);
+    }
+  });
+
+  
+  // add task to tasks array
   const addTaskToArray = (newtask) => {
     const newTask = {
       taskId: Date.now(),
       taskName: newtask,
       isEditMode: false
     }
-
     tasks.value.push(newTask);
   }
+
+
+  // watch() updates the localStorage when tasks array changes.
+  watch(tasks, (updatedTasks) => {
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+  }, { deep: true });
 
 </script>
 
