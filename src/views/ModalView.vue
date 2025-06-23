0b6fa1bac33ed4c249d7ae4448c9d79d14ff35fa
hazbiin <hzbi717@gmail.com>
2025-06-23 11:15:53 +0530

    feat: get taskToUpdate id from url params using useRoute()

--- src/views/ModalView.vue
+++ src/views/ModalView.vue
@@ -1,7 +1,36 @@
 <script setup lang="ts">
     import Modal from '@/components/Modal.vue';
+    import { ref } from 'vue';
+    import { useRoute } from 'vue-router';
+
+    // getting task id from url params
+    const route = useRoute();
+    const taskId = Number(route.params.id);
+
+    // typing task
+    type TaskType = {
+        taskId: number;
+        taskName: string;
+        isEditMode: boolean;
+    }
+
+    // getting task to update from localstorage.
+    const taskToUpdate = ref<TaskType>({} as TaskType);
+    const savedTasks = localStorage.getItem('tasks');
+    if(savedTasks) {
+        taskToUpdate.value = JSON.parse(savedTasks).filter((t: TaskType) => t.taskId === taskId)[0];
+    }
+
+    // note: need to implement this ///////
+    const saveChanges = (updatedTaskName: string) => {
+        console.log(updatedTaskName);
+    }
+
 </script>
 
 <template>
-    <Modal/>
+    <Modal 
+        :taskToUpdate="taskToUpdate"
+        @save-changes="saveChanges"
+    />
 </template>
