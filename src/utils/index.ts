f7127e5f6bf4e72be0f72d1f0d91b7479d312357
hazbiin <hzbi717@gmail.com>
2025-06-30 16:47:33 +0530

    fix: merge conflit push to remote main

---
+++ src/utils/index.ts
@@ -0,0 +1,29 @@
+
+// types
+type TaskType = {
+    id: number;
+    todo: string;
+    completed: boolean;
+    userId: number;
+}
+
+// local storage setting utility function.
+export function setLocalStorage(key: string, value: TaskType[]) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+// fetching from api utility function.
+export async function fetchDataFromApi(url: string, options?: object){
+    try{
+      const response = await fetch(url, options);
+
+      if(!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data;
+    }catch(error) {
+      console.error(`Error fetching data: ${error}`);
+    }
+}
