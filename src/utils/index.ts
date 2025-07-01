7234e6c89e12376caf5dcadb3733b084f3b65b85
hazbiin <hzbi717@gmail.com>
2025-07-01 16:29:40 +0530

    fix: remove unused generic utility fetch function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -20,22 +20,6 @@ export function setLocalStorage(key: string, value: TaskType[]) {
     localStorage.setItem(key, JSON.stringify(value));
 }
 
-// fetching from api utility function.
-export async function fetchDataFromApi(url: string, options?: object){
-    try{
-      const response = await fetch(url, options);
-
-      if(!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      const data = await response.json();
-      return data;
-    }catch(error) {
-      console.error(`Error fetching data: ${error}`);
-    }
-}
-
 // fetch data 
 export async function fetchData(): Promise<TaskType[] | undefined>{
   try {
