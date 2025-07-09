5f9122ebfbdb85bb25117e909d9cc6abad6e639d
hazbiin <hzbi717@gmail.com>
2025-07-09 21:34:09 +0530

    fix: format with prettier

--- src/stores/useTodoListStore.ts
+++ src/stores/useTodoListStore.ts
@@ -1,54 +1,57 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 import type { TaskType } from "@/types";
-import * as util from '@/utils';
+import * as util from "@/utils";
 
-export const useTodoListStore = defineStore('todo-list', () => {
+export const useTodoListStore = defineStore("todo-list", () => {
+  // state
+  const tasks = ref<TaskType[]>([]);
 
-    // state 
-    const tasks = ref<TaskType[]>([]);
-
-    // actions 
-    const readTodos = async(): Promise<void> => {
-        const response = await util.getTodos();
-        if(response) {
-            tasks.value = response;
-        }
+  // actions
+  const readTodos = async (): Promise<void> => {
+    const response = await util.getTodos();
+    if (response) {
+      tasks.value = response;
     }
-    const addTodo = async(newTask: string): Promise<TaskType | undefined> => {
-        const response = await util.addData(newTask);
-        if(response) {
-            tasks.value.push(response);
-            return response;
-        }
+  };
+  const addTodo = async (newTask: string): Promise<TaskType | undefined> => {
+    const response = await util.addData(newTask);
+    if (response) {
+      tasks.value.push(response);
+      return response;
     }
-    const deleteTodo = async (id: number): Promise<TaskType | undefined> => {
-        const response = await util.deleteData(id);
-        if(response) {
-            tasks.value = tasks.value.filter(task => task.id !== response.id);
-            return response;
-        }
+  };
+  const deleteTodo = async (id: number): Promise<TaskType | undefined> => {
+    const response = await util.deleteData(id);
+    if (response) {
+      tasks.value = tasks.value.filter((task) => task.id !== response.id);
+      return response;
     }
-    const updateTodo = async (id: number, updatedTaskName: string): Promise<TaskType | undefined> => {
-        const response = await util.updateData(id, updatedTaskName);
-        if(response) {
-            const indexOfUpdatedItem = tasks.value.findIndex(item => item.id === response.id);
-            if(indexOfUpdatedItem !== -1) {
-                tasks.value.splice(indexOfUpdatedItem, 1, response);
-                return response;
-            }
-        }
+  };
+  const updateTodo = async (
+    id: number,
+    updatedTaskName: string
+  ): Promise<TaskType | undefined> => {
+    const response = await util.updateData(id, updatedTaskName);
+    if (response) {
+      const indexOfUpdatedItem = tasks.value.findIndex(
+        (item) => item.id === response.id
+      );
+      if (indexOfUpdatedItem !== -1) {
+        tasks.value.splice(indexOfUpdatedItem, 1, response);
+        return response;
+      }
     }
+  };
 
-    return { 
+  return {
+    // state
+    tasks,
 
-        // state
-        tasks,
-
-        // actions 
-        readTodos,
-        addTodo,
-        deleteTodo,
-        updateTodo
-    }
+    // actions
+    readTodos,
+    addTodo,
+    deleteTodo,
+    updateTodo,
+  };
 });
